bc554213c950e072e3865c5d41e4f0737c6d2da4
Amin-Mustafa <aminelhag322@gmail.com>
2025-07-28 21:47:05 +0800

    Serial port for debug

---
+++ include/SerialPort.h
@@ -0,0 +1,25 @@
+#ifndef SERIALPORT_H
+#define SERIALPORT_H
+
+#include "MemoryRegion.h"
+#include <vector>
+
+class MMU;
+
+class SerialPort {
+//TODO: make this functional with master and slave gameboys
+//for now this is only for serial debugging e.g. Blargg tests
+public:
+    SerialPort(MMU& mem);
+    uint8_t serial_read(uint16_t addr);
+    void serial_write(uint16_t addr, uint8_t val); 
+    std::vector<uint8_t> destination_buffer;
+private: 
+    //Serial port maps 0xFF01 and 0xFF02
+    MemoryRegion region;  
+    uint8_t serial[2];      
+    //serial[0]: data to be sent
+    //serial[1]: serial control
+};
+
+#endif
---
+++ src/SerialPort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../include/SerialPort.h"
+#include "../include/MMU.h"
+
+uint8_t SerialPort::serial_read(uint16_t addr) {
+    switch(addr) {
+        case 0xFF01: return serial[0];
+        case 0xFF02: return serial[1];
+        default: return 0xFF;
+    }
+}
+
+void SerialPort::serial_write(uint16_t addr, uint8_t val){
+    switch(addr) {
+        case 0xFF01: 
+            serial[0] = val;
+            break;
+        case 0xFF02: 
+            serial[1] = val;
+            //send data to destination if transfer is enabled (bit 7 set)
+            if(val & 0x80) {
+                destination_buffer.push_back(serial[0]); 
+                serial[1] &= ~0x80; //clear bit 7 (transfer complete)
+            }   
+            break;
+        default: break;
+    }
+}
+
+SerialPort::SerialPort(MMU& mem)
+    :region {
+        mem,
+        0xFF01,
+        0xFF02,
+        [this](uint16_t addr){return serial_read(addr);},
+        [this](uint16_t addr, uint8_t val) { serial_write(addr,val); }
+    } {}
